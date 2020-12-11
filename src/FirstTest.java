36006cbafd4a8620a2f0ed7ea2dfe546df9afc47
biruchkovamari <biruchkovamari@gmail.com>
2020-12-12 01:10:32 +0400

    ex2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,11 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -34,7 +37,22 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest(){
-        System.out.println("test text");
+    public void findElementByText(){
+
+        assertElementHasText(By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Search Wikipedia" ,
+                "Input does not have text 'Search Wikipedia'");
     }
+
+    private WebElement waitForElementPresentBy(By by, String error_message, long timeOutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private void assertElementHasText(By by, String value, String error_message){
+        WebElement element = waitForElementPresentBy(by, error_message, 5);
+        Assert.assertEquals(error_message, value, element.getAttribute("text"));
+    }
+
 }
